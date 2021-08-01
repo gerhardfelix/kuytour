df4036e795fe18f016182ecb5ed241900b64fcf5
Gerhard Felix Sitorus <39162121+gerhardfelix@users.noreply.github.com>
2021-08-01 21:29:52 +0700

    Create sidebar.php

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,71 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="<?=base_url('assets');?>/dist/img/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Gerhard F Sitorus</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="active">
+          <a href="<?=base_url('home'); ?>">
+            <i class="fa fa-graduation-cap"></i> <span>Dashboard</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        </li>
+        <li class="active">
+          <a href="<?=base_url('mobil'); ?>">
+            <i class="fa fa-car"></i> <span>Mobil</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        </li>
+        <!-- <li class="active">
+          <a href="<?=base_url('paket'); ?>">
+            <i class="fa fa-map-o"></i> <span>Paket Wisata</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        </li> -->
+        <li class="active">
+          <a href="<?=base_url('supir'); ?>">
+            <i class="fa fa-user-secret"></i> <span>Sopir</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        </li>
+        <li class="active">
+          <a href="<?=base_url('Pesanan'); ?>">
+            <i class="fa fa-file-text"></i> <span>Pesanan</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        <!-- <li class="active">
+          <a href="<?=base_url('laporan'); ?>">
+            <i class="fa fa-bar-chart"></i> <span>Laporan</span>
+            <span class="pull-right-container">
+            </span>
+          </a>
+        </li> -->
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
